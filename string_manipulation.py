285db973637082f8fff18adc64d349da993b1f90
clarence <cmabeya3@gmail.com>
2025-07-22 11:13:46 +0300

    string concatination (using +)

---
+++ string_manipulation.py
@@ -0,0 +1,6 @@
+print("hello" + "world")
+
+print("String manipulation exercise")
+print("string conacatination is done using the '+' sign")
+print("e.g print('hello' + 'jenny')")
+print("new lines can be printed with a backslash and n")
