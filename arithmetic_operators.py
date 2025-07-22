9f1cb6ec88f5a716b98a9b36ebb94a17007b3c2f
clarence <cmabeya3@gmail.com>
2025-07-22 14:17:35 +0300

    arithmetic operators concepts

---
+++ arithmetic_operators.py
@@ -0,0 +1,11 @@
+print(4/2)
+print(4//2)
+print(4**2)
+print(5%2)
+
+
+#calculate BMI = weight/(height**2)
+weight = float(input("enter your weight"))
+height = float(input("enter your height in meters"))
+BMI = weight/(height ** 2)
+print(BMI)
