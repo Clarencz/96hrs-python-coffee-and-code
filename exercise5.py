cad00b4b442fcf7fa8ebbc42036898b99a6a717f
clarence <cmabeya3@gmail.com>
2025-07-23 04:46:11 +0300

    an exercise on f strings

---
+++ exercise5.py
@@ -0,0 +1,8 @@
+age = int(input("what is your age"))
+
+years_left = 90 -age
+days_left=years_left  * 365
+months_left = years_left * 12
+weeks_left = years_left * 52
+
+print(f"you have {years_left} year, {days_left} days, {months_left} months, {weeks_left} weeks")
