f9bbdec1ceef9db03a63f53310e603cedacf30ce
clarence <cmabeya3@gmail.com>
2025-07-23 09:45:58 +0300

    intro to python lists

---
+++ python_lists.py
@@ -0,0 +1,25 @@
+numbers = [10,0,-1,7]
+print(numbers)
+name=["anne","kiei","kiwi","angela"]
+print(name)
+mixed_list = [1,3,4,"one","two"]
+print(mixed_list)
+print(len(name))
+print(numbers[-1])
+print(numbers[-4:-1])
+numbers.sort()
+mixed_list.reverse()
+print(mixed_list)
+numbers.append(34)
+numbers.insert(3,79)
+print(numbers)
+numbers.extend([3,4,5,3,2,1])
+print(numbers)
+numbers[2]= 5.6
+print(numbers)
+numbers[1:5]=[4,23,45,2,90,45]
+print(numbers)
+numbers.pop()
+print(numbers)
+numbers.remove(90)
+print(numbers)
