6d9440b8049ba9d27a1971d5c13cedf9afed3659
clarence <cmabeya3@gmail.com>
2025-07-24 08:15:17 +0300

    difference methods in sets

---
+++ set_methods2.py
@@ -0,0 +1,13 @@
+set1={'ram','akasha','raj','hope'}
+set2={'julie','faith','hope'}
+set3={"anfa","hilda","jane"}
+
+# print(set1.difference(set2))
+# print(set1 - set2)
+# print(set1.difference(set2,set3))
+# print(set1.difference(['ram','raj']))
+# set1.difference_update(set2)
+# print(set1)
+# print(set1.symmetric_difference(set2))
+# print(set1 ^ set2 ^ set3)
+print(set2.symmetric_difference_update(set1))
