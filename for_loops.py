e10014557737f4f5f61bdb5a3ea29002b70d6e8b
clarence <cmabeya3@gmail.com>
2025-07-24 09:41:23 +0300

    intro into for loops

---
+++ for_loops.py
@@ -0,0 +1,13 @@
+# names = ['ram','sham','fiona']
+# for i in names:
+#     print(i)
+#     if i =='sham':
+#         print(f"{i} hey there!")
+        
+numbers = [2,4,5,6,7,8,9]
+squares = []
+for number in numbers:
+    square  = number **2
+    squares.append(square)
+    # print(squares)
+print(squares)
