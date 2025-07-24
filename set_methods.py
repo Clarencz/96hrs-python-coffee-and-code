c5d01bace7a706fca4b5da336555e573d9f33540
clarence <cmabeya3@gmail.com>
2025-07-24 08:00:53 +0300

    union, update and intersection methods in sets

---
+++ set_methods.py
@@ -0,0 +1,15 @@
+set1={'ram','akasha','raj','hope'}
+set2={'julie','faith','hope'}
+set3={"anfa","hilda","jane"}
+
+# union_set = set1.union(set2)#you can use the pipe symbol to show union "|"
+# print(union_set)
+# print(set1 | set2 | set3)
+# print(set1.union(set2,set3))
+# print(set1.union(('jenny','sham')))
+# print(set1.update(set2)) #or set1 =| set2
+# print(set1.intersection(set2))
+# print(set1.intersection(['mohan','lulu']))
+# print(set1 & set2)
+# set1.intersection_update(set2)
+# print(set1)
