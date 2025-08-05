6c53485933cccef2b463508516d8eff1dd9eb369
clarence <cmabeya3@gmail.com>
2025-08-05 17:03:43 +0300

    priority queue

--- intermediate_python/queues.py
+++ intermediate_python/queues.py
@@ -8,8 +8,18 @@ numbers = [10,20,30,40,50,60,70,80,90]
 # print(q.get())
 # print(q.get())
 
-q = queue.LifoQueue()
-for number in numbers:
-    q.put(number)
-print(q.get())
-print(q.get())
+# q = queue.LifoQueue()
+# for number in numbers:
+#     q.put(number)
+# print(q.get())
+# print(q.get())
+
+
+q = queue.PriorityQueue()
+q.put((2,"hello"))
+q.put((11,99))
+q.put((5,7.4))
+q.put((1,True))
+
+while not q.empty():
+    print(q.get())
