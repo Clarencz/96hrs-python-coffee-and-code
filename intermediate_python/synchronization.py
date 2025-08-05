b11f6d97c8de5620f0d8d50946ce8acf8a4f2d50
clarence <cmabeya3@gmail.com>
2025-08-05 16:28:41 +0300

    semaphores.

--- intermediate_python/synchronization.py
+++ intermediate_python/synchronization.py
@@ -1,28 +1,41 @@
 import threading
 import time
-lock  = threading.Lock()
-x = 8192
-def double():
-    global x , lock
-    lock.acquire()
-    while x < 16384:
-        x*=2
-        print(x)
-        time.sleep(2)
-    print("max reached")
-    lock.release()
+# lock  = threading.Lock()
+# x = 8192
+# def double():
+#     global x , lock
+#     lock.acquire()
+#     while x < 16384:
+#         x*=2
+#         print(x)
+#         time.sleep(2)
+#     print("max reached")
+#     lock.release()
     
-def half():
-    global x, lock
-    lock.acquire()
-    while x >1 :
-        x/=2
-        print(x)
-        time.sleep(2)
-    print("min reached")
-    lock.release()
+# def half():
+#     global x, lock
+#     lock.acquire()
+#     while x >1 :
+#         x/=2
+#         print(x)
+#         time.sleep(2)
+#     print("min reached")
+#     lock.release()
     
-double1 = threading.Thread(target=double)
-half1 = threading.Thread(target=half)
-double1.start()
-half1.start()
+# double1 = threading.Thread(target=double)
+# half1 = threading.Thread(target=half)
+# double1.start()
+# half1.start()
+
+
+semaphore = threading.BoundedSemaphore(value=5)
+def access(thread_number):
+    print("{} is trying to access".format(thread_number))
+    semaphore.acquire()
+    print("{} was granted".format(thread_number))
+    time.sleep(10)
+    print(f"{thread_number} is now releasing")
+for thread_number in range(1,15):
+    t = threading.Thread(target=access,args=(thread_number,))
+    t.start()
+    time.sleep(1)
