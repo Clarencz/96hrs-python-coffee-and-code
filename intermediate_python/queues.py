9af51a3a5d186efbb2a09062c1264223352a83f8
clarence <cmabeya3@gmail.com>
2025-08-05 16:58:21 +0300

    lifo queues

--- intermediate_python/queues.py
+++ intermediate_python/queues.py
@@ -1,8 +1,14 @@
 import queue
 
 numbers = [10,20,30,40,50,60,70,80,90]
-q = queue.Queue()
+# q = queue.Queue()
 
+# for number in numbers:
+#     q.put(number)
+# print(q.get())
+# print(q.get())
+
+q = queue.LifoQueue()
 for number in numbers:
     q.put(number)
 print(q.get())
