9e0048985a8ac67a68c48d7a9ea270f46116fd2b
clarence <cmabeya3@gmail.com>
2025-08-06 12:13:10 +0300

    running into recursivw error

--- intermediate_python/logging_.py
+++ intermediate_python/logging_.py
@@ -6,7 +6,24 @@
 
 import logging
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 
-logging.info("emails replied")
-logging.critical("system breach!!")
+# logging.info("emails replied")
+# logging.critical("system breach!!")
+
+logger = logging.getLogger("cmabeya logger")
+# logger.info("admin created logger")
+# logger.critical("admin just removed you")
+# logger.log(logging.ERROR,"an error occured")
+
+
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler("mylog2.log")
+
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter("%(levelname)s - %(asctime)s :%(message)s")
+handler.setFormatter(handler)
+
+logger.addHandler(handler)
+logger.debug("this is an important info")
+# logger.info("this is only basic information") '''check the error on this line...'''
