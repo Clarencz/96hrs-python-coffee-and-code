f27f45859085f4cb094b411ce18cc9b2c2c3fbf5
clarence <cmabeya3@gmail.com>
2025-08-06 09:29:56 +0300

    recursive

--- intermediate_python/recursion.py
+++ intermediate_python/recursion.py
@@ -1,9 +1,22 @@
 # non-recursive
 
-n = 7 
-fact = 1
-while n > 0:
-    fact  = fact * n
-    n -=1
-print(fact)
+# n = 7 
+# fact = 1
+# while n > 0:
+#     fact  = fact * n
+#     n -=1
+# print(fact)
+
+
+
+# recursive
     
+# def factorial(n):
+#     if n<1:
+#         return 1
+#     else:
+#         number = n*factorial(n-1)
+#         return number
+# print(factorial(6))
+
+
