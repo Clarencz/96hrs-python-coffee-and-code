205b746759f5ef589f8599fe6c3ebaffc9b43bf6
clarence <cmabeya3@gmail.com>
2025-08-08 09:54:03 +0300

    using for loops

--- advanced_python/generators.py
+++ advanced_python/generators.py
@@ -1,8 +1,28 @@
-def mygenerator(n):
-    # yield
-    for x in range (n):
-        yield x**3
+import sys
 
-values = mygenerator(3000)
-print(next(values))
-print(next(values))
+# def mygenerator(n):
+#     # yield
+#     for x in range (n):
+#         yield x**3
+
+# values = mygenerator(100)
+# # print(next(values))
+# # print(next(values))
+# print(sys.getsizeof(values))
+
+# # for x in values:
+# #     print(x)
+
+
+def infinite_sequence():
+    result = 1
+    while True:
+        yield result
+        result *= 5
+        
+values = infinite_sequence()
+# print(next(values))
+# print(next(values))
+# print(next(values))
+for x in range(30):
+    print(next(values))
