399302054bb3375e4ab46a0aeeafcc65b38f8b92
clarence <cmabeya3@gmail.com>
2025-08-11 14:20:57 +0300

    documentation

---
+++ python_tips_tricks/docstrings.py
@@ -0,0 +1,12 @@
+def myExponent(num1,num2):
+    '''
+    the function takes one number to the power of annother and returns the result
+    :param num1: base
+    :param num2 : exponent
+    :return: the result
+    '''
+    return num1 ** num2
+
+# print(myExponent(34,2))
+print(help(myExponent))
+print(myExponent.__doc__)
