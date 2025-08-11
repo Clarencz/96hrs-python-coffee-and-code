bb776e067ccb2594b13f0e009b2f498fca72c728
clarence <cmabeya3@gmail.com>
2025-08-11 14:00:52 +0300

    list reversion

---
+++ python_tips_tricks/reversing_lists.py
@@ -0,0 +1,19 @@
+values = [1,2,3,4,5,6,7,8,9] 
+
+revlist = []
+
+# ======METHOD 1=====
+# for index in range(len(values)):
+#     revlist.append(values[len(values) - index - 1])
+    
+# print(revlist)
+
+# ========METHOD 2=======
+# values.reverse()
+# print(values)
+
+# ========METHOD 3 ==========
+# print(list(reversed(values)))
+
+# ==========METHOD 4 ===========
+# print(values[::-1])
