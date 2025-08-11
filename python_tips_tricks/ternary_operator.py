1bbf916b98f926c136048a370b8f7700996c8914
clarence <cmabeya3@gmail.com>
2025-08-11 08:08:21 +0300

    ternary operators

---
+++ python_tips_tricks/ternary_operator.py
@@ -0,0 +1,15 @@
+# age = 34
+
+# adult = True if age >= 40 else False
+
+# print("you are adult" if adult else "you are not an adult")
+# if adult:
+#     print("you are an adult")
+# else:
+#     print("you are not an adult")
+
+
+# =======example 2 ============
+
+number = 30 
+print("this number is very large" if number > 100 else "this number is quite large"if number > 50 else "this number is small but still positive")
