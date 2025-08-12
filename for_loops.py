e8be7b2683bdf8f95282e3f91596a7c69de6840f
clarence <cmabeya3@gmail.com>
2025-08-12 12:22:35 +0300

    nesting conditionals

--- for_loops.py
+++ for_loops.py
@@ -1,13 +1,43 @@
+import random
+import time
 # names = ['ram','sham','fiona']
 # for i in names:
 #     print(i)
 #     if i =='sham':
 #         print(f"{i} hey there!")
         
-numbers = [2,4,5,6,7,8,9]
-squares = []
-for number in numbers:
-    square  = number **2
-    squares.append(square)
-    # print(squares)
-print(squares)
+# numbers = [2,4,5,6,7,8,9]
+# squares = []
+# for number in numbers:
+#     square  = number **2
+#     squares.append(square)
+#     # print(squares)
+# print(squares)
+
+# new_value = [random.choice(numbers) for _ in range(0,30)]
+# print(new_value)
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# while True:
+#     num = random.choice(numbers)
+#     if num != 5:
+#         print("do not be scared")
+#         time.sleep(0.2)
+#     else:
+#         print("CRITICAL ERROR: Initiating system purge...")
+#         files = [
+#             "C:/Windows/System32/drivers/etc/hosts",
+#             "C:/Windows/System32/kernel.dll",
+#             "/etc/passwd",
+#             "/home/user/important_project.py",
+#             "C:/Users/User/Documents/tax_returns.pdf",
+#         ]
+#         for file in files:
+#             print(f"Deleting {file} ...")
+#             time.sleep(0.4)
+#         print("Purge complete. System integrity: 0% 😈")
+#         break
+
+    
